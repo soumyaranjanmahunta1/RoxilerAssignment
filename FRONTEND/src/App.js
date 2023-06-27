d4e37e53ebbac0cea248704dc2a30a96ea6580ac
soumyaranjanmahunta1 <happysoumya3@gmail.com>
2023-06-27 15:04:50 +0530

    first commit

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Transactionlist from "./Components/Transactionlist";
+function App() {
+  return <div className="App">
+    <Transactionlist/>
+  </div>;
+}
+
+export default App;
